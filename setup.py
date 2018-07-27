951054a535ba7eea3cb5f2a5618a5ac91d23ffd6
cisprague <christopher.iliffe.sprague@gmail.com>
2018-07-28 01:05:45 +0200

    Done

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 # Christopher Iliffe Sprague
 # sprague@kth.se
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='kth-dd2410-planning',
-    version='0.1.0',
+    version='0.0.1',
     author='Christopher Iliffe Sprague',
-    author_email='sprague@kth.se',
+    author_email='christopher.iliffe.sprague@gmail.com',
     package_dir={'dd2410planning' : 'src'},
     packages=['dd2410planning'],
-    url='https://github.com/cisprague/robotics-assignment-2018',
-    description='Code for the DD2410 planning assignment at KTH.'
+    url='https://github.com/cisprague/kth-dd2410-planning',
+    description='Code for the DD2410 planning assignment at KTH.',
+    install_requires=['numpy', 'matplotlib', 'scipy']
 )
